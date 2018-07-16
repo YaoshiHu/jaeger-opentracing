69042cda9c1754792ea0fad47c8f8857bb1a2ab3
yaoshi <henryhustone@outlook.com>
2018-07-15 22:30:14 -0400

    helloTracing

---
+++ helloTracing/hello.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/opentracing/opentracing-go/log"
+	"github.com/YaoshiHu/jaeger-opentracing/initTracer"
+)
+
+func main() {
+	sampleTracer, closer := initTracer.SimpleTracer("Hello World")
+	defer closer.Close()
+
+	span := sampleTracer.StartSpan("hello to world")
+	defer span.Finish()
+
+	span.LogFields(
+		log.String("event", "Hello Begin"),
+	}
+
+	fmt.Println("Hello World")
+
+	span.LogFields(
+		log.String("event", "Hello End"),
+	}
+
+}
